c908bcf56a27dfdc9a39bc7d5fe68d485a9ee030
azurejelly <178000437+azurejelly@users.noreply.github.com>
2025-08-07 18:23:27 -0400

    fix: automatically disable when the plugin serves no function

--- src/main/java/dev/azuuure/villagerfix/VillagerFix.java
+++ src/main/java/dev/azuuure/villagerfix/VillagerFix.java
@@ -11,25 +11,42 @@ public final class VillagerFix extends JavaPlugin {
 
     private Logger logger;
     private PluginManager pluginManager;
+    private int hooks;
 
     @Override
     public void onLoad() {
+        this.hooks = 0;
         this.logger = getLogger();
         this.pluginManager = getServer().getPluginManager();
     }
 
     @Override
     public void onEnable() {
+        if (!pluginManager.isPluginEnabled("VillagerInABukkit")) {
+            logger.severe("Failed to find VillagerInABukkit. Disabling VillagerFix.");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+
         if (pluginManager.isPluginEnabled("GriefPrevention")) {
-            logger.info("Registered GriefPrevention event listeners");
+            hooks++;
             pluginManager.registerEvents(new GriefPreventionListener(logger), this);
+            logger.info("Registered GriefPrevention event listeners");
         }
 
         if (pluginManager.isPluginEnabled("WorldGuard")) {
-            logger.info("Registered WorldGuard event listeners");
+            hooks++;
             pluginManager.registerEvents(new WorldGuardListener(logger), this);
+            logger.info("Registered WorldGuard event listeners");
         }
 
+        if (hooks == 0) {
+            logger.severe("No compatible protection plugins were found. Disabling VillagerFix.");
+            pluginManager.disablePlugin(this);
+            return;
+        }
+
+        logger.info("Hooked into " + hooks + " plugin(s).");
         logger.info("VillagerFix version " + getDescription().getVersion() + " has been enabled.");
         logger.info("Made by https://github.com/azurejelly");
     }
